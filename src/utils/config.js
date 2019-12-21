3679e9fce7a81b0725511f3fe4e19e3d237872c4
Victor Molero <victor.molero@gmail.com>
2019-12-21 21:06:52 +0100

    read NODE_ENV

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
+const NODE_ENV = process.env.NODE_ENV;
 
-if (process.env.NODE_ENV === 'test') {
+if (NODE_ENV === 'test') {
   MONGODB_URI =
     process.env.MONGODB_TEST || 'mongodb://root:root@localhost:27017/test';
 }
@@ -12,5 +13,6 @@ if (process.env.NODE_ENV === 'test') {
 module.exports = {
   MONGODB_URI,
   PORT,
-  SECRET
+  SECRET,
+  NODE_ENV
 };
