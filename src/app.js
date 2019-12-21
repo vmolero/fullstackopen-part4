8859f7df1ddbe39dd6f44d5781bfe452063992f5
Victor Molero <victor.molero@gmail.com>
2019-12-21 21:06:01 +0100

    use morgan here and tokenExtractor

--- src/app.js
+++ src/app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const config = require('./utils/config');
 const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
-
 const app = express();
+
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -19,8 +23,21 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
-app.use(middleware.requestLogger);
+if (config.NODE_ENV !== 'test') {
+  morgan.token('morganBody', function(req) {
+    if ('body' in req && !isEmptyObject(req.body)) {
+      return JSON.stringify(req.body);
+    }
+    return ' ';
+  });
+  app.use(
+    morgan(
+      ':method :url :status :res[content-length] - :response-time ms :morganBody'
+    )
+  );
+}
 
+app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
